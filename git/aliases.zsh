adbe24c15eb9a1d6f7d17bb8fff9c236a05c078f
Lorin Hochstein <lhochstein@netflix.com>
2021-11-06 17:50:29 -0700

    checkpoint

---
+++ git/aliases.zsh
@@ -0,0 +1,13 @@
+
+alias br="git branch --show-current | tr -d '\n' | pbcopy && pbpaste"
+alias c="git add . && git commit -m 'checkpoint'"
+alias init="touch README.md && git init && git add ."
+alias files="git files"
+alias fgit="SPOTLESS_SKIP=1 git"
+alias g="SPOTLESS_SKIP=1 git"
+alias gitconfig="code ~/.gitconfig"
+alias gg="SPOTLESS_SKIP=1 git"
+alias lg=lazygit
+alias pull="git pull"
+alias pr="gh pr checkout"
+alias sw="git switch -"
