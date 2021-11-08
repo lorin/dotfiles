dd0254572acdfe4c754e7cfae5c749d66e53792f
Lorin Hochstein <lhochstein@netflix.com>
2021-11-07 22:22:36 -0800

    checkpoint

--- search/tag.zsh
+++ search/tag.zsh
@@ -1,6 +1,8 @@
 # for https://github.com/aykamko/tag
 # uses "ag" as the alias, but it's ripgrep underneath
 
+# This doesn't work, and I don't know why
+# I've worked around it by aliasing vim to nvim
 export TAG_CMD_FORMAT_STRING="nvim -c 'call cursor({{.LineNumber}}, {{.ColumnNumber}})' '{{.Filename}}'"
 
 if (( $+commands[tag] )); then
