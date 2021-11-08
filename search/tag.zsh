412f791ae7db98b16d165502bc7afe32f4c45a18
Lorin Hochstein <lhochstein@netflix.com>
2021-11-07 22:20:00 -0800

    checkpoint

--- search/tag.zsh
+++ search/tag.zsh
@@ -1,7 +1,11 @@
 # for https://github.com/aykamko/tag
 # uses "ag" as the alias, but it's ripgrep underneath
+
+export TAG_CMD_FORMAT_STRING="nvim -c 'call cursor({{.LineNumber}}, {{.ColumnNumber}})' '{{.Filename}}'"
+
 if (( $+commands[tag] )); then
   export TAG_SEARCH_PROG=rg  # replace with rg for ripgrep
   tag() { command tag "$@"; source ${TAG_ALIAS_FILE:-/tmp/tag_aliases} 2>/dev/null }
   alias ag=tag  # replace with rg for ripgrep
 fi
+
