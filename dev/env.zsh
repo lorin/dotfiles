7e3d42d2804d525c85e64ddcdd0dcc3259b4b03f
Lorin Hochstein <lhochstein@netflix.com>
2021-12-20 10:25:20 -0800

    checkpoint

---
+++ dev/env.zsh
@@ -0,0 +1,5 @@
+# workaround for issue with lein and Ultra, c.f.:
+#
+# https://github.com/brandonbloom/fipp/issues/60
+# https://clojure.atlassian.net/browse/CLJ-2077
+export LEIN_USE_BOOTCLASSPATH=no
