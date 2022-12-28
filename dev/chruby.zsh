b099602da10aa5e61ed938ce312a445eb227596f
Lorin Hochstein <lhochstein@netflix.com>
2022-12-27 19:11:37 -0800

    add chruby

---
+++ dev/chruby.zsh
@@ -0,0 +1,4 @@
+# from https://jekyllrb.com/docs/installation/macos/
+unalias ls  ## script uses ls -A, which exa doesn't support
+source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh
+source $(brew --prefix)/opt/chruby/share/chruby/auto.sh
